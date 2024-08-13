df2a3b078871816856666b4fd16f3fd0f855240d
avdhutssh <avdhut.ssh@gmail.com>
2024-08-13 18:14:24 +0530

    SRP -> Page Object -> Google Main Page

---
+++ src/main/java/com/seleniumdesign/srp/GoogleMainPage.java
@@ -0,0 +1,28 @@
+package com.seleniumdesign.srp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class GoogleMainPage {
+    private WebDriver driver;
+    private SearchWidget searchWidget;
+    private SearchSuggestion searchSuggestion;
+
+     public GoogleMainPage(final WebDriver driver){
+         this.driver = driver;
+         this.searchWidget = PageFactory.initElements(driver,SearchWidget.class);
+         this.searchSuggestion = PageFactory.initElements(driver, SearchSuggestion.class);
+     }
+
+     public void goTo(){
+         driver.get("https://www.google.com");
+    }
+
+    public SearchWidget getSearchWidget() {
+        return searchWidget;
+    }
+
+    public SearchSuggestion getSearchSuggestion() {
+        return searchSuggestion;
+    }
+}
