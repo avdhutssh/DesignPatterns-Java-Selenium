7ba8c28cbf1fede3e16738c0c4f0834cc34b1f85
avdhutssh <avdhut.ssh@gmail.com>
2024-08-13 18:15:05 +0530

    SRP -> Create Test -> Google Test

---
+++ src/test/java/com/seleniumdesign/srp/test/GoogleTest.java
@@ -0,0 +1,43 @@
+package com.seleniumdesign.srp.test;
+
+import com.seleniumdesign.srp.GoogleMainPage;
+import com.seleniumdesign.srp.GoogleResultPage;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class GoogleTest extends BaseTest{
+
+    private GoogleMainPage googleMainPage;
+    private GoogleResultPage googleResultPage;
+
+    @BeforeTest
+    public void setUpPages(){
+        this.googleMainPage = new GoogleMainPage(driver);
+        this.googleResultPage = new GoogleResultPage(driver);
+    }
+
+    @Test
+    public void googleWorkflow(){
+        String keyword = "selenium webdriver";
+        int index  = 3;
+
+        googleMainPage.goTo();
+        Assert.assertTrue(googleMainPage.getSearchWidget().isDisplayed());
+
+        googleMainPage.getSearchWidget().enter(keyword);
+        Assert.assertTrue(googleMainPage.getSearchSuggestion().isDisplayed());
+
+        googleMainPage.getSearchSuggestion().clickSuggestionByIndex(index);
+        Assert.assertTrue(googleResultPage.getNavigationBar().isDisplayed());
+
+        googleResultPage.getSearchWidget().enter(keyword);
+        Assert.assertTrue(googleResultPage.getSearchSuggestion().isDisplayed());
+
+        googleResultPage.getSearchSuggestion().clickSuggestionByIndex(index);
+
+        googleResultPage.getNavigationBar().goToNews();
+
+        System.out.println("Total search result are: "+googleResultPage.getResultStat().getStat());
+    }
+}
