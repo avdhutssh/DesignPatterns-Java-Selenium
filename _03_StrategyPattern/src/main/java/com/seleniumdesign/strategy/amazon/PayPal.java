e987090ec0814dbad9888deb92cefc0fe2a7dd17
avdhutssh <avdhut.ssh@gmail.com>
2024-08-22 13:07:06 +0530

    Strategy Pattern -> Create Payment Component -> Pay Pal

---
+++ _03_StrategyPattern/src/main/java/com/seleniumdesign/strategy/amazon/PayPal.java
@@ -0,0 +1,20 @@
+package com.seleniumdesign.strategy.amazon;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.Map;
+
+public class PayPal implements PaymentOption {
+    @FindBy(id = "paypal_username")
+    private WebElement username;
+
+    @FindBy(id = "paypal_password")
+    private WebElement password;
+
+    @Override
+    public void enterPaymentInformation(Map<String, String> paymentDetails) {
+        this.username.sendKeys(paymentDetails.get("username"));
+        this.password.sendKeys(paymentDetails.get("pwd"));
+    }
+}
