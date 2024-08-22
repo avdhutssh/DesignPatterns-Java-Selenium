248992652faad5160b4717edc3e77d549b31b724
avdhutssh <avdhut.ssh@gmail.com>
2024-08-22 13:07:28 +0530

    Strategy Pattern -> Create Test -> Payment Screen Test

---
+++ _03_StrategyPattern/src/test/java/com/seleniumdesign/strategy/test/PaymentScreenTest.java
@@ -0,0 +1,52 @@
+package com.seleniumdesign.strategy.test;
+
+import com.seleniumdesign.strategy.amazon.*;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.collections.Maps;
+
+import java.util.Map;
+
+public class PaymentScreenTest extends BaseTest {
+    private PaymentScreen paymentScreen;
+
+    @BeforeTest
+    public void setPaymentScreen() {
+        this.paymentScreen = new PaymentScreen(this.driver);
+    }
+
+    @Test(dataProvider = "getData")
+    public void paymentTest(PaymentOption paymentOption, Map<String, String> paymentDetails) {
+        paymentScreen.goTo();
+        paymentScreen.getUserInformation().enterDetails("Avdhut", "Shirgaonkar", "avd123@gmail.com");
+        paymentScreen.setPaymentOption(paymentOption);
+        paymentScreen.pay(paymentDetails);
+        String orderNumber = paymentScreen.getOrder().placeOrder();
+        System.out.println("Order Number for " + paymentOption + " : " + orderNumber);
+    }
+
+    @DataProvider
+    public Object[][] getData() {
+        Map<String, String> cc = Maps.newHashMap();
+        cc.put("cc", "1231231231");
+        cc.put("year", "2023");
+        cc.put("cvv", "123");
+
+        Map<String, String> nb = Maps.newHashMap();
+        nb.put("bank", "BOFA");
+        nb.put("account", "myaccount123");
+        nb.put("pin", "999");
+
+        Map<String, String> pp = Maps.newHashMap();
+        pp.put("username", "Avdhut");
+        pp.put("pwd", "avdhut@7766");
+
+        return new Object[][]{
+                {new CreditCard(), cc},
+                {new NetBanking(), nb},
+                {new PayPal(), pp}
+        };
+
+    }
+}
