61e538393c5bfe9c8d0dc68e0bcdf17528653a5b
avdhutssh <avdhut.ssh@gmail.com>
2024-08-22 20:20:10 +0530

    Command Pattern -> Create Validator -> Notification Validator

---
+++ _04_CommandPattern/src/main/java/com/seleniumdesign/command/NotificationValidator.java
@@ -0,0 +1,26 @@
+package com.seleniumdesign.command;
+
+import com.google.common.util.concurrent.Uninterruptibles;
+import org.openqa.selenium.WebElement;
+
+import java.util.concurrent.TimeUnit;
+
+public class NotificationValidator extends ElementValidator {
+
+    private final WebElement button;
+    private final WebElement notification;
+
+    public NotificationValidator(final WebElement button, final WebElement notification) {
+        this.button = button;
+        this.notification = notification;
+    }
+
+    @Override
+    public boolean validate() {
+        this.button.click();
+        boolean appearanceState = this.notification.isDisplayed();
+        Uninterruptibles.sleepUninterruptibly(4, TimeUnit.SECONDS);
+        boolean disappearanceState = this.notification.isDisplayed();
+        return appearanceState && !disappearanceState;
+    }
+}
