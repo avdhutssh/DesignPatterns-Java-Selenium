a23d86be8d00148c2444d13d220eb36d306c1c12
avdhutssh <avdhut.ssh@gmail.com>
2024-08-23 20:05:16 +0530

    Proxy Pattern -> Create Components -> User Information

---
+++ _06_ProxyPattern/src/main/java/com/seleniumdesign/proxy/UserInformation.java
@@ -0,0 +1,27 @@
+package com.seleniumdesign.proxy;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class UserInformation {
+    @FindBy(id = "fn")
+    private WebElement firstName;
+
+    @FindBy(id = "ln")
+    private WebElement lastName;
+
+    @FindBy(id = "email")
+    private WebElement email;
+
+    public UserInformation(final WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    public void enterDetails(String fn, String ln, String email) {
+        firstName.sendKeys(fn);
+        lastName.sendKeys(ln);
+        this.email.sendKeys(email);
+    }
+}
