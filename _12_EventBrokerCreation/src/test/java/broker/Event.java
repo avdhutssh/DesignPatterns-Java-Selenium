f074f65991526408fa593af086d72e59f0d98c8c
avdhutssh <avdhut.ssh@gmail.com>
2024-11-13 21:29:28 +0530

    EventBroker uml creation -> Event class

---
+++ _12_EventBrokerCreation/src/test/java/broker/Event.java
@@ -0,0 +1,55 @@
+package broker;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Event {
+    private final String type;
+    private final Object body;
+    private final long timeout;
+    private final int priority;
+    private final String sender;
+    private final Map<String, Object> headers;
+    private final Date time;
+
+    private Event() {
+        this.type = null;
+        this.body = null;
+        this.timeout = 0;
+        this.priority = 0;
+        this.sender = null;
+        this.headers = new HashMap<>();
+        this.time = new Date();
+    }
+
+    // Getters
+    public String getType() {
+        return type;
+    }
+
+    public Object getBody() {
+        return body;
+    }
+
+    public long getTimeout() {
+        return timeout;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public Map<String, Object> getHeaders() {
+        return Collections.unmodifiableMap(headers);
+    }
+
+    public Date getTime() {
+        return (Date) time.clone();
+    }
+}
